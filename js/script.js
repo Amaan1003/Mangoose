8759e536bc499140881f91ebb5c23b594db0bb5a
Amaan1303 <amanullah.mansuri@vpt.edu.in>
2025-01-26 21:47:15 +0530

    update
    upd 2

--- js/script.js
+++ js/script.js
@@ -25,3 +25,45 @@ menuButton.addEventListener("click", () => {
     ? 'MENU <span>&#9660;</span>'
     : 'CLOSE <span>&#9650;</span>';
 });
+
+
+const carousel = document.querySelector('.carousel');
+const slides = document.querySelectorAll('.carousel-slide');
+let currentIndex = 0;
+
+function autoSlide() {
+  currentIndex++;
+  if (currentIndex >= slides.length) {
+    currentIndex = 0; // Reset to the first slide
+  }
+  carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+// Auto-slide every 3 seconds
+setInterval(autoSlide, 3000);
+
+
+// Select the red dot element
+const redDot = document.getElementById("red-dot");
+
+// Mouse move event
+document.addEventListener("mousemove", (event) => {
+  // Show the dot
+  redDot.style.display = "block";
+
+  // Update the position of the dot considering scroll offsets
+  redDot.style.left = `${event.clientX + window.scrollX}px`;
+  redDot.style.top = `${event.clientY + window.scrollY}px`;
+});
+
+// Touch event for mobile
+document.addEventListener("touchstart", (event) => {
+  const touch = event.touches[0];
+
+  // Show the dot
+  redDot.style.display = "block";
+
+  // Update the position of the dot considering scroll offsets
+  redDot.style.left = `${touch.clientX + window.scrollX}px`;
+  redDot.style.top = `${touch.clientY + window.scrollY}px`;
+});
